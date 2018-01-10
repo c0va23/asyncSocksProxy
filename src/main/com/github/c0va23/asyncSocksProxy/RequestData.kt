b107d31e9d12c449dbf35a3757f721ca99696105
Dmitrij Fedorenko <c0va23@gmail.com>
2018-01-10 10:10:21 +0300

    Refactor Command and RequestData for specs

--- src/main/com/github/c0va23/asyncSocksProxy/RequestData.kt
+++ src/main/com/github/c0va23/asyncSocksProxy/RequestData.kt
@@ -2,30 +2,22 @@ package com.github.c0va23.asyncSocksProxy
 
 import java.net.InetAddress
 
-abstract class RequestData(
-        val address: InetAddress,
-        val port: Int,
+interface RequestData {
+        val address: InetAddress
+        val port: Int
         val command: Command
-)
+}
 
-class Socks4RequestData(
-        address: InetAddress,
-        port: Int,
-        command: Command,
+data class Socks4RequestData(
+        override val address: InetAddress,
+        override val port: Int,
+        override val command: Command,
         val userId: String
 
-) : RequestData(
-        address = address,
-        port = port,
-        command = command
-)
+) : RequestData
 
-class Socks5RequestData(
-        address: InetAddress,
-        port: Int,
-        command: Command
-) : RequestData(
-        address = address,
-        port = port,
-        command = command
-)
+data class Socks5RequestData(
+        override val address: InetAddress,
+        override val port: Int,
+        override val command: Command
+) : RequestData
